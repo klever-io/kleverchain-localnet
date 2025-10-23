a89cb58dca4de896ae1e0beddec485f0ce0863c5
nickgs1337 <nicklatim@live.com>
2025-10-23 12:43:45 -0300

    Reduce waittime on nodeSetup.json configuration

--- scripts/generate_configs.py
+++ scripts/generate_configs.py
@@ -30,8 +30,8 @@ def generate_genesis(wallets,klv_supply,path):
 
 def generate_nodes_setup(wallets, validators, path):
     start_time = int(datetime.now().timestamp())
-    start_time = int(start_time) + 500 - (int(start_time) % 500)
-    chain_id = start_time // 500
+    start_time = int(start_time) + 75 - (int(start_time) % 75)
+    chain_id = start_time // 75
     
     slot_per_epoch = 20
     
