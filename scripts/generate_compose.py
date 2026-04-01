0510a905fdcb761ed22c0d2851e42a592ac03194
Beroni <beronimagalhaes@gmail.com>
2026-04-01 16:58:33 -0300

    fix: prefix bind-mount paths with ./ to prevent Docker named-volume misinterpretation

--- scripts/generate_compose.py
+++ scripts/generate_compose.py
@@ -3,11 +3,11 @@ import platform
 from const import _COMPOSER_NODES,_COMPOSER_BASE
 
 def generate_compose(validators):
-    nodes_data = ""
+    nodes_data = []
     index = 0
     is_windows = platform.system() == 'Windows'
 
-    for _,value in validators.items():
+    for value in validators.values():
         index_with_zero = index if index >= 10 else f"0{index}"
 
         # Use relative path so docker-compose works on any OS/user
@@ -19,13 +19,13 @@ def generate_compose(validators):
             if len(path) > 1 and path[1] == ':':
                 drive = path[0].lower()
                 path = f"/{drive}{path[2:]}"
+        else:
+            path = f"./{path}"
 
-        nodes_data += _COMPOSER_NODES % (index, index, index_with_zero, index_with_zero, path, path, index, index, index_with_zero)
-        index = index + 1
+        nodes_data.append(_COMPOSER_NODES % (index, index, index_with_zero, index_with_zero, path, path, index, index, index_with_zero))
+        index += 1
 
-    compose = _COMPOSER_BASE % nodes_data
+    compose = _COMPOSER_BASE % ''.join(nodes_data)
 
-
-    # Dump the dictionary to a .yaml file
     with open('docker-compose.yaml', 'w') as file:
         file.write(compose)
